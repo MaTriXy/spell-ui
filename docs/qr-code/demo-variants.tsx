ad00b6adad3d11582c95c995397f764dda2a88f0
xxtomm <3xteam.dev@gmail.com>
2026-03-12 01:21:16 +0900

    feat: add qr-code component

---
+++ docs/qr-code/demo-variants.tsx
@@ -0,0 +1,32 @@
+import { QRCode } from "@/registry/spell-ui/qr-code";
+
+export function Demo() {
+  return (
+    <div className="flex flex-wrap gap-6">
+      <div className="w-[105px] md:w-[140px] rounded-lg p-2 shadow-[0_0_0_1px_rgba(0,0,0,.08),_0px_2px_2px_rgba(0,0,0,.04)] dark:border [&_svg]:w-full [&_svg]:h-auto">
+        <QRCode
+          value="https://spell.sh"
+          size={140}
+          fgColor="#c2410c"
+          bgColor="white"
+        />
+      </div>
+      <div className="w-[105px] md:w-[140px] rounded-lg p-2 shadow-[0_0_0_1px_rgba(0,0,0,.08),_0px_2px_2px_rgba(0,0,0,.04)] dark:border [&_svg]:w-full [&_svg]:h-auto">
+        <QRCode
+          value="https://spell.sh"
+          size={140}
+          fgColor="#1d4ed8"
+          bgColor="white"
+        />
+      </div>
+      <div className="w-[105px] md:w-[140px] rounded-lg p-2 shadow-[0_0_0_1px_rgba(0,0,0,.08),_0px_2px_2px_rgba(0,0,0,.04)] dark:border [&_svg]:w-full [&_svg]:h-auto">
+        <QRCode
+          value="https://spell.sh"
+          size={140}
+          fgColor="#15803d"
+          bgColor="white"
+        />
+      </div>
+    </div>
+  );
+}
---
+++ docs/qr-code/demo.tsx
@@ -0,0 +1,9 @@
+import { QRCode } from "@/registry/spell-ui/qr-code";
+
+export function Demo() {
+  return (
+    <div className="w-[105px] md:w-[140px] rounded-lg p-2 shadow-[0_0_0_1px_rgba(0,0,0,.08),_0px_2px_2px_rgba(0,0,0,.04)] dark:border [&_svg]:w-full [&_svg]:h-auto">
+      <QRCode value="https://spell.sh" size={140} />
+    </div>
+  );
+}
